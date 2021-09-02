932c3465f3807095cac85ac4d5d5e4898f71ed24
pontakornth <pontakorn61@gmail.com>
2021-09-02 21:23:35 +0700

    feat: Add route for individual post

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const PostDetail: NextPage = () => {
+	return (
+		<div className="detail">
+			<h1>Post title</h1>
+		</div>
+	)
+}
+
+export default PostDetail

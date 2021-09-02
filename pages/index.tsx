96bade173ccb28ee3c7b681a1442f533b8e7ddd0
pontakornth <pontakorn61@gmail.com>
2021-09-02 21:38:22 +0700

    feat: Implement home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
+import Link from 'next/link'
+import { Post } from '../@types/Post'
 
-const Home: NextPage = () => {
+type HomeProps = {
+  postList: Post[]
+}
+
+const Home: NextPage<HomeProps> = ({postList}) => {
   return  (
     <div className="main">
       <h1>Fake Blog</h1>
+      <ul>
+        {postList.map(post => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context)  => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const json = await response.json()
+  return {
+    props: {
+      postList: json,
+    }
+  }
 }
 
 export default Home

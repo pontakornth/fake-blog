d9358381a83c8960563750239894d72ab88036a0
pontakornth <pontakorn61@gmail.com>
2021-09-03 13:29:05 +0700

    feat: Add comments to the post page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,38 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { Post, Comment } from '../../@types/Post'
 
-const PostDetail: NextPage = () => {
+type PostDetailProps = {
+	post: Post,
+	comments: Comment[]
+}
+
+const PostDetail: NextPage<PostDetailProps> = ({ post, comments }) => {
 	return (
 		<div className="detail">
-			<h1>Post title</h1>
+			<h1>{post.title}</h1>
+			<p>{post.body}</p>
+			<h2>Comments</h2>
+			<ul>
+				{comments.map(comment => (
+					<li key={comment.id}>{comment.body}</li>
+				))}
+			</ul>
 		</div>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const postId: string = context.params!['id'] as unknown as string
+	const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+	const commentsResponsee = await fetch(`https://jsonplaceholder.typicode.com/comments/?postId=${postId}`)
+	const post: Post = await postResponse.json()
+	const comments: Comment[] = await commentsResponsee.json()
+	return {
+		props: {
+			post,
+			comments,
+		}
+	}
 }
 
 export default PostDetail
